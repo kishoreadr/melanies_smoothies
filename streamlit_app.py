1aa58ea7e49bfd46186a9b41adfa44955a775f2a
kishoreadr <kishore@adroitts.com>
2025-10-16 20:04:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARC
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 
-pd_df = my_dataframe.toPandas
+pd_df = my_dataframe.toPandas()
 st.dataframe(pd_df)
 st.stop()
 
